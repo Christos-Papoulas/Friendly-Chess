6d5e233feb2c0f8ea0113addf471a65e1144c278
Christos Papoulas <pachristos@programize.com>
2016-05-22 16:43:20 +0300

    logic for threatening

--- friendly_chess.py
+++ friendly_chess.py
@@ -9,10 +9,38 @@ from friendly_chess.board import Board
 def main():
     """ Main of the project. """
     print("started")
-    board = Board(3, 3)
-    print(board)
+    M = 3
+    N = 3
+    free_board = Board(M, N)
+    pawns = ['K', 'K', 'R']
+    findSolutions(free_board, pawns, M, N)
     print("finished")
     return
+
+
+def findSolutions(board, pawns, M, N):
+    """ Find the solution boards. """
+    if len(pawns) == 0:
+        print(board)
+        print('\n')
+        return
+
+    b = None
+    for p in pawns:
+        pawns_new = pawns[:]
+        pawns_new.remove(p)  # remove the candidate
+        secure_pos = board.get_secure_positions()
+
+        for s in secure_pos:
+            tx, ty = s
+            b = Board(board.sizeX, board.sizeY, board.board)
+            if b.is_pawn_threats_others(p, tx, ty) is True:
+                continue
+            b.set_pawn_in(p, tx, ty)
+            findSolutions(b, pawns_new, M, N)
+
+    return board
+
 
 if __name__ == '__main__':
     main()
--- friendly_chess/board.py
+++ friendly_chess/board.py
@@ -1,12 +1,19 @@
 """ The board of chess. """
 
+THREAT = 'T'
+
 
 class Board(object):
     """ The board of chess. """
 
-    def __init__(self, M, N):
+    def __init__(self, M, N, data=None):
         """ Initiaze the board. """
-        self.board = [[" " for _ in range(N)] for _ in range(M)]
+        if data is None:
+            self.board = [[" " for _ in range(N)] for _ in range(M)]
+        else:
+            self.board = data
+        self.sizeX = M
+        self.sizeY = N
 
     def __str__(self):
         """ Print the Board. """
@@ -20,3 +27,118 @@ class Board(object):
     def set_pawn_in(self, pawn, x_pos, y_pos):
         """ Set the pawn in position x_pos, y_pos. """
         self.board[x_pos][y_pos] = pawn
+
+    def get_secure_positions(self):
+        """ Return all free positions. """
+        self.calculate_threats()
+        return [(ix, iy) for ix, row in enumerate(self.board)
+                for iy, i in enumerate(row) if i == ' ']
+
+    def set_position_as_thread(self, x_pos, y_pos):
+        """ Set the position x_pos, y_pos as thread. """
+        if self.board[x_pos][y_pos] == ' ':
+            self.board[x_pos][y_pos] = THREAT
+
+    def calculate_threats(self):
+        """ Add the threts to the board. """
+        for x in range(0, self.sizeX):
+            for y in range(0, self.sizeY):
+                if self.board[x][y] == 'K':
+                    self.calculate_threats_for_king(x, y)
+                elif self.board[x][y] == 'Q':
+                    self.calculate_threats_for_queen(x, y)
+                elif self.board[x][y] == 'B':
+                    self.calculate_threats_for_bishop(x, y)
+                elif self.board[x][y] == 'R':
+                    self.calculate_threats_for_rook(x, y)
+                elif self.board[x][y] == 'N':
+                    self.calculate_threats_for_knight(x, y)
+
+    def is_pawn_threats_others(self, pawn, x, y):
+        """ Return True if the pawn in x, y threats any one else. """
+        if pawn == 'K':
+            return self.check_threats_for_king(x, y)
+        elif pawn == 'Q':
+            return self.check_threats_for_queen(x, y)
+        elif pawn == 'B':
+            return self.check_threats_for_bishop(x, y)
+        elif pawn == 'R':
+            return self.check_threats_for_rook(x, y)
+        elif pawn == 'N':
+            return self.check_threats_for_knight(x, y)
+        assert True, "Unknown issue in is_pawn_threats_others"
+
+    def calculate_threats_for_king(self, x, y):
+        """ Add the threads to the board for the king. """
+        for i in [-1, 0, 1]:
+            if x + i >= 0 and x + i < self.sizeX:
+                for j in [-1, 0, 1]:
+                    if y + j >= 0 and y + j < self.sizeY:
+                        self.set_position_as_thread(x + i, y + j)
+
+    def calculate_threats_for_rook(self, x, y):
+        """ Add the threads to the board. """
+        for i in range(0, self.sizeX):
+            # print("i: " + str(i))
+            if self.board[i][y] == ' ':
+                self.set_position_as_thread(i, y)
+
+        for j in range(0, self.sizeY):
+            # print("j: " + str(j))
+            if self.board[x][j] == ' ':
+                self.set_position_as_thread(x, j)
+        return
+
+    def calculate_threats_for_queen(self, x, y):
+        """ Add the threads to the board. """
+        assert True, "Not implemented"
+        return
+
+    def calculate_threats_for_bishop(self, x, y):
+        """ Add the threads to the board. """
+        assert True, "Not implemented"
+        return
+
+    def calculate_threats_for_knight(self, x, y):
+        """ Add the threads to the board. """
+        assert True, "Not implemented"
+        return
+
+    def check_threats_for_king(self, x, y):
+        """ Return true if threat other pawns. """
+        for i in [-1, 0, 1]:
+            if x + i >= 0 and x + i < self.sizeX:
+                for j in [-1, 0, 1]:
+                    if y + j >= 0 and y + j < self.sizeY:
+                        content = self.board[x + i][y + j]
+                        if content != ' ' and content != THREAT:
+                            return True
+        return False
+
+    def check_threats_for_rook(self, x, y):
+        """ Return true if threat other pawns. """
+        for i in range(0, self.sizeX):
+            # print("i: " + str(i))
+            if self.board[i][y] != ' ' and self.board[i][y] != THREAT:
+                return True
+
+        for j in range(0, self.sizeY):
+            # print("j: " + str(j))
+            if self.board[x][j] != ' ' and self.board[x][j] != THREAT:
+                return True
+        return False
+
+    def check_threats_for_queen(self, x, y):
+        """ Return true if threat other pawns. """
+        assert True, "Not implemented"
+        return
+
+    def check_threats_for_bishop(self, x, y):
+        """ Return true if threat other pawns. """
+        assert True, "Not implemented"
+        return
+
+    def check_threats_for_knight(self, x, y):
+        """ Return true if threat other pawns. """
+        assert True, "Not implemented"
+        return
